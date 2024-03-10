35ba26188eb2ebe24a1eaf82abd1fd7094a85b42
aashishnepal <aashishnepal50@gmail.com>
2024-03-10 22:31:17 +0545

    fix:chrome drive on main.py

--- main.py
+++ main.py
@@ -11,18 +11,17 @@ import chromedriver_autoinstaller
 import time
 import csv
 
-chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
-                                      # and if it doesn't exist, download it automatically,
-                                      # then add chromedriver to path
+chromedriver_autoinstaller.install() 
+                                   
 
 options = webdriver.ChromeOptions()
-# options.add_argument('--headless')
-# options.add_argument('--enable-javascript') # 启用 JavaScript
-# options.add_argument('blink-settings=imagesEnabled=false')      # 不加载图片，提升运行速度
-# options.add_argument('--no-sandbox')                # 解决DevToolsActivePort文件不存在的报错
-# options.add_argument('--disable-gpu')               # 谷歌文档提到需要加上这个属性来规避bug
-# options.add_argument('--hide-scrollbars')           # 隐藏滚动条，应对一些特殊页面
-# options.add_argument("--headless") #无界面
+options.add_argument('--headless')
+options.add_argument('--enable-javascript')  
+options.add_argument('blink-settings=imagesEnabled=false')     
+options.add_argument('--no-sandbox')               
+options.add_argument('--disable-gpu')              
+options.add_argument('--hide-scrollbars')          
+options.add_argument("--headless")  
 
 url='https://merolagani.com/'
 driver = webdriver.Chrome(options=options)
